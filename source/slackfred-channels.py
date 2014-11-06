018e0a2302fbdffb15aa50669b744d981360e779
Frank <frank@spinillo.net>
2014-11-05 20:30:08 -0500

    Argparse fixes

--- source/slackfred-channels.py
+++ source/slackfred-channels.py
@@ -2,6 +2,14 @@ import sys
 import argparse
 from workflow import Workflow, ICON_WEB, web, PasswordNotFound
 
+def getChannelName(api_key, query):
+    channelsList = web.get('https://slack.com/api/channels.list?token=' + api_key + '&pretty=1').json()
+
+    for channels in channelsList['channels']:
+        if channels['id'] == query:
+            channelName = channels['name']
+    return channelName
+
 def slackChannels(api_key):
 
     channels = web.get('https://slack.com/api/channels.list?token=' + api_key + '&count=50&pretty=1').json()
@@ -16,6 +24,12 @@ def searchSlackChannels(channels):
 
 def main(wf):
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--join', nargs = '?')
+    parser.add_argument('--leave', dest = 'leave', nargs = '?')
+    parser.add_argument('query', nargs = '?', default = None)
+    args = parser.parse_args(wf.args)
+
     try:
         api_key = wf.get_password('slack_api_key')
     except PasswordNotFound:
@@ -25,15 +39,20 @@ def main(wf):
         wf.send_feedback()
         return 0
 
+    if args.leave:
+        query = args.leave
+        web.get('https://slack.com/api/channels.leave?token=' + api_key + '&channel=' + query + '&pretty=1')
+    elif args.join:
+        query = args.join
+        channelName = getChannelName(api_key, query)
+        web.get('https://slack.com/api/channels.join?token=' + api_key + '&name=' + channelName + '&pretty=1')
+
     def wrapper():
         return slackChannels(api_key)
 
-    channelsList = wf.cached_data('channels', wrapper, max_age = 180)
+    channelsList = wf.cached_data('channels', wrapper, max_age = 60)
 
-    if len(wf.args):
-        query = wf.args[0]
-    else:
-        query = None
+    query = args.query
 
     if query:
         channelsList = wf.filter(query, channelsList, key = searchSlackChannels)
